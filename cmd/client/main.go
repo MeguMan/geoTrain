abbca23e4ce230cdc45d0f0a1be615060d9d39a2
MeguMan <dimon200019@gmail.com>
2020-12-15 13:20:10 +0300

    refactor client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,9 +8,6 @@ func main() {
 	c := client.NewClient("http://localhost:8080")
 	c.Login("supersecretpassword")
 	c.Set("mykesasmdy2", "myvaluLKABDe", 0)
-	c.Get("mykesasmdy2")
 	c.HSet("myhash22", "myfield3", "myvalue4")
-	c.HGet("myhash22", "myfield3")
 	c.Delete("mykesasmdy2")
-	c.Save()
 }
